32652a4449a15207f5d9d82e6b84fe613a04e354
Lenastartscoding <lena.frommann@gmx.de>
2021-03-09 10:04:35 +0100

    finished stage 1

--- ctf-en/stage1.py
+++ ctf-en/stage1.py
@@ -3,16 +3,13 @@ import json
 
 network_url = "https://api.meraki.com/api/v1/organizations/549236/networks"
 base_url = "https://api.meraki.com/api/v1"
-global orga_id
-global network_id
-device_dict = dict()
 device_list = list()
 payload = None
 
 headers = {
-"Content-Type": "application/json",
-"Accept": "application/json",
-"X-Cisco-Meraki-API-Key": "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+    "X-Cisco-Meraki-API-Key": "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
 }
 
 response_network = requests.request('GET', url = network_url , headers=headers, data = payload)
@@ -31,8 +28,17 @@ response_device_json = response_device.json()
 
 #print(response_device_json)
 for line in response_device_json:
-    if 'name' in line:
-        print("Device name : "+line['name']+"; Mac: "+line['mac']+"; Serial: "+line['serial']+ "; Model: "+line['model'])
-    else:
-        print("Device name : No device Name"+"; Mac: "+line['mac']+"; Serial: "+line['serial']+ "; Model: "+line['model'])
+    device_dict = dict()
+    for variable in ['name','model','mac','serial']:
+        if variable in line: 
+            device_dict[variable] = line[variable]
+        else:
+            device_dict[variable] = 'N/A'
+    #print(device_dict)
+    device_list.append(device_dict)
+output_list = device_list
 
+print(output_list)
+
+with open("file_device.json", "w") as file:
+    json.dump(output_list, file)
